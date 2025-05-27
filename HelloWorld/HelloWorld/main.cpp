5b2647b62e0157b55841f5845a4c976a6ea7477f
Jordan Hwang <jordanwhwang@gmail.com>
2025-05-27 01:16:51 -0400

    C++20
    Using std::format

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+#include <format>
 
 int main()
 {
-	std::cout << "Heyoo!! It's Jordy!\n";
+	int x;
+	int y;
+
+	std::cout << "Enter the first number: ";
+	std::cin >> x;
+	std::cout << "Enter the second number: ";
+	std::cin >> y;
+
+	int z = x + y;
+	std::string sentence = std::format("{} + {} = {}", x, y, z);
+
+	std::cout << sentence << '\n';
 	return 0;
 }
